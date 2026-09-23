98573d156582fbecc741ba94b8e771a78ae5a9c2
Bugra Celik <celik.bugra@outlook.de>
2026-09-23 23:07:09 +0200

    Practice SQLite queries and enum mapping

--- GameBacklog.Cli/Data/SqliteDatabase.cs
+++ GameBacklog.Cli/Data/SqliteDatabase.cs
@@ -1,3 +1,4 @@
+using GameBacklog.Cli.Enums;
 using Microsoft.Data.Sqlite;
 
 namespace GameBacklog.Cli.Data;
@@ -50,6 +51,94 @@ internal class SqliteDatabase
 
             Console.WriteLine(
             $"{id} | {title} | {platform} | {state} | {rating} | {playTimeHours}");
+        }
+    }
+
+    public void ShowTitles()
+    {
+        using SqliteConnection connection = 
+            new SqliteConnection(_connectionString);
+
+        connection.Open();
+
+        string sql = """
+            SELECT Title
+            FROM Games;
+            """;
+
+        using SqliteCommand command =
+            new SqliteCommand(sql, connection);
+
+        using SqliteDataReader reader =
+            command.ExecuteReader();
+
+        while(reader.Read())
+        {
+            string title = reader.GetString(0);
+
+            Console.WriteLine($"Game title: {title}");
+        }
+    }
+
+    public void ShowRatings()
+    {
+        using SqliteConnection connection =
+            new SqliteConnection(_connectionString);
+
+        connection.Open();
+
+        string sql = """
+            SELECT Title, Rating
+            FROM Games;
+            """;
+
+        using SqliteCommand command =
+            new SqliteCommand(sql, connection);
+
+        using SqliteDataReader reader =
+            command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            string title = reader.GetString(0);
+            int rating = reader.GetInt32(1);
+
+            Console.WriteLine($"Title: {title} | Rating: {rating}");
+        }
+    }
+
+    public void ShowHighlyRatedGames()
+    {
+        using SqliteConnection connection =
+            new SqliteConnection(_connectionString);
+
+        connection.Open();
+
+        string sql = """
+            SELECT Title,
+                   Platform,
+                   State,
+                   Rating,
+                   PlayTimeHours
+            FROM Games
+            WHERE Rating >= 9;
+            """;
+
+        using SqliteCommand command =
+            new SqliteCommand(sql, connection);
+
+        using SqliteDataReader reader =
+            command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            string title = reader.GetString(0);
+            Platform platform = (Platform)reader.GetInt32(1);
+            GameState state = (GameState)reader.GetInt32(2);
+            int rating = reader.GetInt32(3);
+            double playTimeHours = reader.GetDouble(4);
+
+            Console.WriteLine($"Title: {title} | State: {state} | Platform: {platform} | Rating: {rating} | Hours played: {playTimeHours}");
         }
     }
 }
